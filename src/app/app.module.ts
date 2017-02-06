43ae10702230e63eb69261201ba38c439ae96933
Ced c <cedvandenbosch@gmail.com>
2017-02-06 19:13:49 +0100

    comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Ng2DialogComponent } from './dialog/ng2-dialog.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -7,7 +8,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ng2DialogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dialog/ng2-dialog.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+	selector: 'ng2-dialog',
+	template: `<div (click)="close()" *ngIf="open" class="modal">
+								<div class="dlg" (click)="$event.stopPropagation()">
+									<div class="closeIc" (click)="close()" *ngIf="closeable">&#10005;</div>
+									<div class="content"><ng-content></ng-content></div>
+								</div>
+						 <div>
+	`,
+	styles: [`
+		.dlg{
+			margin: auto;
+			padding: 0;
+			z-index: 9999;
+			background: #fff;
+			display:inline-block;
+			border-radius: 0.3rem;
+    	box-shadow: -1px 2px 9px #787878;
+		}
+		.modal{
+			z-index: 9998;
+			background: rgba(0,0,0,0.3);
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			display: flex;
+		}
+		.closeIc{
+			color: #aaa;
+			text-align: right;
+			cursor: pointer;
+			margin-right: 0.4rem;
+			float: right;
+		}
+		.closeIc:hover{
+			color: #555;
+		}
+		.content{
+			margin: 1rem;
+		}
+	`]
+})
+export class Ng2DialogComponent implements OnInit {
+	private _open:boolean;
+	@Output() private closed = new EventEmitter<boolean>();
+	@Input() private closeable = true;
+
+	constructor() { }
+
+	ngOnInit() { }
+
+	close(){
+		this.closed.emit(true);
+	}
+
+	get open(){
+		return this._open;
+	}
+
+	@Input()
+	set open(open){
+		this._open = open;
+	}
+
+}
